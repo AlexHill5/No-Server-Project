3d3d37de131ca0514bc06233ec3d7dd5edad047b
Alex Hill <big_al_hill@comcast.net>
2017-06-21 15:26:22 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from "./Components/Nav-bar/Navbar.js"
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar />
+    )
+  }
+}
+
+export default App;
